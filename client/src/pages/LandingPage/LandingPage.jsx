001b30e4587540e1e7f32bd7a32b1f1fdc41566a
z <prospectheory@gmail.com>
2024-04-05 00:09:58 +0800

    improve styles

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import styles from "./LandingPage.module.css";
 export default function LandingPage() {
   return (
     <div className={styles.wrapper}>
-      <h1>Chat app</h1>
+      <h1>Real-time chat app</h1>
       <div className={styles.actions}>
         <LoginButton>Login</LoginButton>
         <SignupButton>Sign up</SignupButton>
