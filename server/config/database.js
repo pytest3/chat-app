67a0d70c320954ad6319dde634f986f891ebec42
z <prospectheory@gmail.com>
2024-04-04 15:59:26 +0800

    fix env variables not avialble to npx cli

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,24 @@
 // we will need to tell the CLI how to connect to the database
 // by providing db credentials here
 
+require("dotenv").config({ path: __dirname + "/../.env" });
+
+console.log("development config", {
+  username: process.env.USERNAME,
+  password: null,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  dialect: process.env.DIALECT || "postgres",
+  logging: false,
+});
+
 module.exports = {
   development: {
     username: process.env.USERNAME,
     password: null,
     database: process.env.DATABASE,
     host: process.env.HOST,
-    dialect: process.env.DIALECT,
+    dialect: process.env.DIALECT || "postgres",
     logging: false,
   },
   production: {
@@ -21,14 +32,3 @@ module.exports = {
     logging: true, // Enable logging
   },
 };
-
-console.log("production config lol", {
-  username: process.env.PROD_DB_USERNAME,
-  password: process.env.PROD_DB_PASSWORD,
-  // database name
-  database: process.env.PROD_DB_NAME,
-  // database url
-  host: process.env.PROD_DB_HOSTNAME,
-  dialect: process.env.PROD_DB_DIALECT || "postgres",
-  logging: true, // Enable logging
-});
